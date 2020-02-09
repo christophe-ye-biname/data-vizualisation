3f566cc06b78493adea4932f0a0fa5c579bbd510
christophe-ye-biname <christopheyebiname@gmail.com>
2020-02-09 18:07:51 +0100

    random

--- data-vizualisation.py
+++ data-vizualisation.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 from random import choice
-from random_walk import RandomWalk
 
 plt.scatter(2, 4, s=200)
 
@@ -59,3 +58,17 @@ rw = RandomWalk()
 rw.fill_walk()
 plt.scatter(rw.x_values, rw.y_values, s=15)
 plt.show()
+
+####################################
+
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+
+    pts_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=pts_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    plt.show()
+
+    keep_running = input("Make another? (y/n)")
+    if keep_running == "n":
+        break
